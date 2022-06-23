90b8a3f2d2b7ad02981211051f56d0626f568da2
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-23 11:59:39 -0500

    navbar added

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,11 @@ router.get('/google/callback', passport.authenticate('google', {failureRedirect:
 }
 )
 
+//@desc logout user
+//@route /auth/logout
+router.get('/logout', (req, res)=> {
+    req.logout()
+    res.redirect('/')
+})
+
 module.exports = router
