37344fcb1b4dc777725e2f093a70ac6065a9bb47
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-22 20:07:41 -0500

    basic routes set up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 //load config
@@ -15,6 +16,14 @@ if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+//HandleBars
+// app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Routes
+app.use('/', require('./routes/index'))
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
