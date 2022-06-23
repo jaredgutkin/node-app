1e7ffdd3e3e845041f13589e9b53c523cab5ff76
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-23 16:08:53 -0500

    edit stories

--- app.js
+++ app.js
@@ -28,14 +28,15 @@ if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 //handlebar helpers
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 //HandleBars
 app.engine('.hbs', exphbs.engine({ helpers: { 
         formatDate,
         stripTags,
         truncate,
-        editIcon, 
+        editIcon,
+        select,
     }, 
     defaultLayout: 'main', 
     extname: '.hbs'}));
